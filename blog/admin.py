5f4a2d8032c6cd7ad559dd246260f135f3ccb951
kabangu <kabangufuture@gmail.com>
2025-07-13 20:52:37 +0200

    Add blog app models and admin configuration

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from blog.models import Tag, Post
+
+
+admin.site.register(Tag)
+admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title", "published_at", )}
+    list_display = ('slug', 'published_at')
